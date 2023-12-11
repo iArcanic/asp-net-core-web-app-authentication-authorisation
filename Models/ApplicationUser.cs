45f1221416855d722a0711c6620f5498dbe66990
Preetham Ananthkumar <preetham.mervin@gmail.com>
2023-12-11 20:11:23 +0000

    Added ApplicationUser class

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace asp_net_core_web_app_authentication_authorisation.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public String FirstName { get; set; } = "";
+        public String LastName { get; set; } = "";
+        public String Address { get; set; } = "";
+        public DateTime CreatedAt { get; set; }
+    }
+}
--- Services/ApplicationDbContext.cs
+++ Services/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using asp_net_core_web_app_authentication_authorisation.Models;
 
 namespace asp_net_core_web_app_authentication_authorisation.Services
 {
-    public class ApplicationDbContext: IdentityDbContext
+    public class ApplicationDbContext: IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions options) : base(options)
         { 
