2342891a0a3a04d7c6ced0babb83af8c35d23b9b
Preetham Ananthkumar <preetham.mervin@gmail.com>
2023-12-11 20:06:10 +0000

    Removed migrations and installed Microsoft.AspNetCore.Identity.EntityFrameworkCore Nuget package to use IdentityDb context

--- Services/ApplicationDbContext.cs
+++ Services/ApplicationDbContext.cs
@@ -1,8 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace asp_net_core_web_app_authentication_authorisation.Services
 {
-    public class ApplicationDbContext: DbContext
+    public class ApplicationDbContext: IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions options) : base(options)
         { 
