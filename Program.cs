d208f7266f867d6813c0ab54cc84f6343cb27f41
Preetham Ananthkumar <preetham.mervin@gmail.com>
2023-12-11 19:53:33 +0000

    Added ApplicationDbContext and migration file

--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
+using asp_net_core_web_app_authentication_authorisation.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
---
+++ Services/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace asp_net_core_web_app_authentication_authorisation.Services
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        { 
+            
+        }
+    }
+}
