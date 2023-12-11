65b96e9e146a98e6823530df9eac6e23a456797f
Preetham Ananthkumar <preetham.mervin@gmail.com>
2023-12-11 20:19:53 +0000

    Added migration with necessary AspNet tables and relevant identity roles

--- Services/ApplicationDbContext.cs
+++ Services/ApplicationDbContext.cs
@@ -1,14 +1,31 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using asp_net_core_web_app_authentication_authorisation.Models;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+using Microsoft.AspNetCore.Identity;
 
 namespace asp_net_core_web_app_authentication_authorisation.Services
 {
     public class ApplicationDbContext: IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions options) : base(options)
-        { 
-            
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            var admin = new IdentityRole("admin");
+            admin.NormalizedName = "admin";
+
+            var client = new IdentityRole("client");
+            client.NormalizedName = "client";
+
+            var seller = new IdentityRole("seller");
+            seller.NormalizedName = "seller";
+
+            builder.Entity<IdentityRole>().HasData(admin, client, seller);
         }
     }
 }
