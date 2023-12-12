1bd5abc92181cc3215edecf3274c3d3d0e152948
Preetham Ananthkumar <preetham.mervin@gmail.com>
2023-12-12 11:02:32 +0000

    Added additional fields to Register page

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -71,6 +71,18 @@ namespace asp_net_core_web_app_authentication_authorisation.Areas.Identity.Pages
         /// </summary>
         public class InputModel
         {
+            [Required]
+            public string FirstName { get; set; }
+
+            [Required]
+            public string LastName { get; set; }
+
+            [Required]
+            public string PhoneNumber { get; set; }
+
+            [Required]
+            public string Address { get; set; }
+
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
